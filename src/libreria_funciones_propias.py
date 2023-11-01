d63a2685d0a5f3aa8a7cb01a42bf5a308d5a1ebd
Juan Antonio Tora Canovas <juanantonio.tora@gmail.com>
2023-11-01 04:08:21 +0100

    WIP - raspado detalle pelicula en proceso y modularizado

--- src/libreria_funciones_propias.py
+++ src/libreria_funciones_propias.py
@@ -1,41 +1,190 @@
 from requests_html import HTMLSession
 import requests
-import os
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from lxml import etree
+from dtos import ClasePeliculaDTO
+import csv
 
-def consigueCodigoFuente_si_noHTML(url):
-    """
-    Consigue el HTML de código fuente de un sitio web, tras renderizar el JavaScript.
-        Params:
-            url: La URL del sitio web
-        Returns:
-            html: Código fuente en HTML tras renderizar el JavaScript
-    """
-    session =HTMLSession()
-    resp = session.get(url)
-    resp.html.render()
-    soup = BeautifulSoup(resp.html.html, "html.parser")
-    html =soup.prettify()
 
-    nombre_archivo = url.replace("https://","")
-    nombre_archivo = nombre_archivo.replace("/", "-")
-    nombre_archivo = nombre_archivo.replace(".", "_")
-    archivo = open("",nombre_archivo,".txt","w")
-    archivo.write(str((html)))
-    archivo.close()
+
+def return_html_after_scrape_movie_info_from_summary_page(browser, num_clics):
+    """
+    Obtiene el código fuente HTML tras la ejecución de JavaScript y extrae los datos de las películas.
+    Parámetros:
+        browser: Con los datos de la conexión HTTP establecida y la web cargada.
+        num_clics: El número de veces para hacer clic en la flecha para cargar más películas.
+                   Por defecto, hacemos clic 33 veces para cargar 1020 películas.
+    Return:
+        html: El código fuente HTML como una cadena de texto.
+    """
+    
+    
+    # Bucle para hacer 33 veces scroll hacia abajo y click en la flecha que con javascrip sigue mostrando más películas
+    for i in range(num_clics):
+        # Esperamos para evitar sobrecargar el servidor
+        time.sleep(3)
+
+        # Hacemos clic en la flecha para cargar más películas
+        button = browser.find_element(By.ID, "load-more-bt")
+        browser.execute_script("arguments[0].scrollIntoView(true);", button)
+        browser.execute_script("arguments[0].click();", button)
+    
+    # Esperamos para que las últimas 30 películas carguen
+    time.sleep(3)
+    
+    # Obtenemos todo el código fuente HTML de la página con las 1000-1030 películas ya cargadas, incluidos los cambios realizados por JavaScript
+    html = browser.page_source
+    
+    # Cerramos el navegador para liberar recursos
+    browser.quit()
+
     return html
 
 
-def captura_detalle_pelicula_HTML(url, nombre_txt):
-    response = requests.get(url)
-    html = BeautifulSoup(response.content, "html.parser")
 
-    with open(str(nombre_txt), "w") as f:
-        for dd in html.find_all("dd", class_="", itemprop="description"):
-            print(dd.text)
-            f.write(str(dd.text))
+
+
+def recoger_detalles_concretos_pelicula_HTML(url, nombre_txt, obj_pelicula):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    html = soup.prettify()
+
+    with open(str(nombre_txt), "w",  encoding="utf-8") as f:
+        f.write(html)
+
+    # Consiguiendo valor de "Título original"
+    div = soup.find(id="left-column")
+    dl  = div.find("dl", class_="movie-info")
+    dd = dl.find("dd")
+    value = dd.text
+    value = value.replace("\t", "")
+    value = value.replace(" ", "")
+    value = value.replace("\n", "")
+    print("Título original: ",value)
+
+     # Consiguiendo valor de "Título original"
+    div = soup.find(id="left-column")
+    dl  = div.find("dl", class_="movie-info")
+    dd = dl.find_all("dd")[2]
+    value = dd.text
+    print("Duración: ",value)
+
+    # Conseguimos los generos
+    #div = soup.find("div", id="left-column")
+    #dd  = div.find("dd", _class="card-genres")
+    #generos = []
+    #for span in dd.findall("span", itemprop="genre"):
+    #    a = span.find(a)
+    #    value = a.value
+    #    generos.append(value)
+    #print("Géneros: ", generos)
+
+    # Conseguimos la sinopsis
+    sinopsis = soup.find("dd", class_="", itemprop="description")
+    value = sinopsis.text
+    print("Sinopsis: ", value)
+
+
+
+
+def capture_data_from_sumaryPage(obj_pelicula):
+    """
+    A partir del HTML extraido, se localizan los datos de interés y se escriben en un fichero CSV.
+    Parámetros:
+        html: El código fuente en HTML que se extrajo de la web.
+    Return:
+        obj_detalles_peliculas: objeto de tipo "ClasePeliculasDTO"
+    """
     
+    # Creamos listas para guardar los valores de cada variable
+    movie_titles = []
+    movie_years = []
+    movie_countries = []
+    movie_ratings = []
+    movie_rating_counts = []
+    movie_directors = []
+    movie_cast = []
+    movie_links = []
     
+    # Analizamos el HTML con lxml
+    root = etree.HTML(str(obj_pelicula.get_html()))
+    
+    # Recopilo datos: recorro todos los div's con la clase "mc-info-container"
+    for div in root.xpath('//div[@class="mc-info-container"]'):
+        title_div = div.xpath('.//div[@class="mc-title"]')[0]
+        full_text = title_div.xpath('string(.)').strip()
+        
+        # Obtenemos el título y el año
+        title, year = full_text.rsplit('(', 1)
+        year = year.rstrip(')')
+        title = title.strip()
+        
+        # Obtenemos el país de origen
+        country = title_div.xpath('.//img[@class="nflag"]/@alt')[0]
+        
+        # Obtenemos el director y el reparto
+        director = div.xpath('.//div[@class="mc-director"]/div[@class="credits"]/span/a/text()')
+        cast = div.xpath('.//div[@class="mc-cast"]/div[@class="credits"]/span/a/text()')
+        
+        # Obtenemos el enlace de la película
+        link = div.xpath('.//div[@class="mc-title"]/a/@href')[0]
+        
+        movie_titles.append(title)
+        movie_years.append(year)
+        movie_countries.append(country)
+        movie_directors.append(", ".join(director))
+        movie_cast.append(", ".join(cast))
+        movie_links.append(link)
+    
+    # Recopilo datos: recorro todos los li's con la clase "data"
+    for li in root.xpath('//li[@class="data"]'):
+        # Extraemos la puntuación media y el número de puntuaciones
+        rating = li.xpath('.//div[@class="avg-rating"]/text()')[0]
+        rating_count = li.xpath('.//div[@class="rat-count"]/text()')[0].strip()
+        movie_ratings.append(rating)
+        movie_rating_counts.append(rating_count)
+    
+    # Paso los datos recopilados al objeto de retorno
+    obj_pelicula.set_movie_titles(movie_titles)
+    obj_pelicula.set_movie_years(movie_years)
+    obj_pelicula.set_movie_countries(movie_countries)
+    obj_pelicula.set_movie_ratings(movie_ratings)
+    obj_pelicula.set_movie_rating_counts(movie_rating_counts)
+    obj_pelicula.set_movie_directors(movie_directors)
+    obj_pelicula.set_movie_cast(movie_cast)
+    obj_pelicula.set_movie_links(movie_links)
+    return obj_pelicula
+
+def write_data_in_csv(obj_pelicula):
+
+    # Escribimos todo en un archivo CSV
+    with open('dataset_movie_info.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Título', 'Año', 'País', 'Puntuación Media', 'Número de Puntuaciones', "Director", "Reparto", "Enlace", "Título Original", "Duración", "Género", "Sinopsis"])
+
+        for i, (title, year, country, rating, rating_count, director, cast, link, originalTitle, duration, gender, synopsis) in enumerate(zip(obj_pelicula.get_movie_titles(), 
+                                                                                                   obj_pelicula.get_movie_years(), 
+                                                                                                   obj_pelicula.get_movie_countries(), 
+                                                                                                   obj_pelicula.get_movie_ratings(), 
+                                                                                                   obj_pelicula.get_movie_rating_counts(), 
+                                                                                                   obj_pelicula.get_movie_directors(), 
+                                                                                                   obj_pelicula.get_movie_cast(), 
+                                                                                                   obj_pelicula.get_movie_links(), 
+                                                                                                   obj_pelicula.get_titulo_original(), 
+                                                                                                   obj_pelicula.get_duracion(), 
+                                                                                                   obj_pelicula.get_genero(), 
+                                                                                                   obj_pelicula.get_sinopsis())
+                                                                                                ):
+            # Detenemos el proceso si hemos escrito 1000 filas
+            if i >= 1000:
+                break
+            writer.writerow([title, year, country, rating, rating_count, director, cast, link, originalTitle, duration, gender, synopsis])
+    
+
 
 if __name__ == "__main__":
-    captura_detalle_pelicula_HTML("https://www.filmaffinity.com/es/film893369.html", "fichero.txt")
+    obj_detalles_peliculas = ClasePeliculaDTO("",[],[],[],[],[],[],[],[],[],[],[],[])
+    recoger_detalles_concretos_pelicula_HTML("https://www.filmaffinity.com/es/film893369.html", "fichero.txt", obj_detalles_peliculas)
--- src/main.py
+++ src/main.py
@@ -4,166 +4,92 @@ import time
 from lxml import etree
 import csv
 from dtos import ClasePeliculaDTO
-from libreria_funciones_propias import captura_detalle_pelicula_HTML, consigueCodigoFuente_si_noHTML
-
-
-def login_FillmAffinity_And_Navegation_To_TopFA(url):
-    # Abrir navegador Firefox y acceder a URL general
-    options = webdriver.FirefoxOptions()
-    options.headless = True
-    
-    # Instanciamos navegador Firefox con opciones definidas y navegamos a la URL deseada
-    browser = webdriver.Firefox(options=options)
-    browser.get(url)
-    time.sleep(5)
-
-     # Buscamos el botón «ACEPTO» y lo clicamos
-    button = browser.find_element(By.CLASS_NAME, "css-v43ltw")
-    button.click()
-
-    # navegación a la sección "TopFA"
-    irTopFA = browser.find_element(By.XPATH, '/html/body/header/div[2]/div/ul/li[1]/a').click()
-    time.sleep(3)
-    
-    # Cargo la página "TopFA" con filtro de búsqueda concreto
-    browser.get("https://www.filmaffinity.com/es/topgen.php?genres=&chv=0&orderby=avg&movietype=movie%7C&country=&fromyear=2013&toyear=2023&ratingcount=3&runtimemin=0&runtimemax=4")
-    return browser
-    
+from libreria_funciones_propias import return_html_after_scrape_movie_info_from_summary_page, recoger_detalles_concretos_pelicula_HTML, capture_data_from_sumaryPage, write_data_in_csv
+from threading import Thread
 
 
 
-def return_html_after_scrape_movie_info_from_summary_page(browser, num_clics):
-    """
-    Obtiene el código fuente HTML tras la ejecución de JavaScript y extrae los datos de las películas.
-    Parámetros:
-        browser: Con los datos de la conexión HTTP establecida y la web cargada.
-        num_clics: El número de veces para hacer clic en la flecha para cargar más películas.
-                   Por defecto, hacemos clic 33 veces para cargar 1020 películas.
-    Return:
-        html: El código fuente HTML como una cadena de texto.
-    """
-    
-    
-    # Bucle para hacer 33 veces scroll hacia abajo y click en la flecha que con javascrip sigue mostrando más películas
-    for i in range(num_clics):
-        # Esperamos para evitar sobrecargar el servidor
-        time.sleep(3)
+def capture_details_from_all_filmPage(obj_pelicula):
+    links = obj_pelicula.get_movie_links()
+    cantidadLink = len(links)
+    division = cantidadLink / 30
+    i = 0
+    pos_links = 0
+    while i < division:
+        hilos = []
+        for i in range(30):
+            obj_pelicula = hilos.append(Thread(target=recoger_detalles_concretos_pelicula_HTML, args=(str(links[pos_links]), "ficheroDesdeMain.txt", obj_pelicula)))
 
-        # Hacemos clic en la flecha para cargar más películas
-        button = browser.find_element(By.ID, "load-more-bt")
-        browser.execute_script("arguments[0].scrollIntoView(true);", button)
-        browser.execute_script("arguments[0].click();", button)
-    
-    # Esperamos para que las últimas 30 películas carguen
-    time.sleep(3)
-    
-    # Obtenemos todo el código fuente HTML de la página con las 1000-1030 películas ya cargadas, incluidos los cambios realizados por JavaScript
-    html = browser.page_source
-    
-    # Cerramos el navegador para liberar recursos
-    browser.quit()
+        for hilo in hilos:
+            hilo.start()
 
-    return html
+        for hilo in hilos:
+            hilo.join()
 
-
-
-
-def write_in_file_the_dataset(obj):
-    """
-    A partir del HTML extraido, se localizan los datos de interés y se escriben en un fichero CSV.
-    Parámetros:
-        html: El código fuente en HTML que se extrajo de la web.
-    Return:
-        obj_detalles_peliculas: objeto de tipo "ClasePeliculasDTO"
-    """
-
-    # Analizamos el HTML con lxml
-    root = etree.HTML(obj.get_html())
-    
-    # Creamos listas para guardar los valores de cada variable
-    movie_titles = []
-    movie_years = []
-    movie_countries = []
-    movie_ratings = []
-    movie_rating_counts = []
-    movie_directors = []
-    movie_cast = []
-    movie_links = []
-    
-    # Recorremos todos los divs con la clase "mc-info-container"
-    for div in root.xpath('//div[@class="mc-info-container"]'):
-        title_div = div.xpath('.//div[@class="mc-title"]')[0]
-        full_text = title_div.xpath('string(.)').strip()
-        
-        # Obtenemos el título y el año
-        title, year = full_text.rsplit('(', 1)
-        year = year.rstrip(')')
-        title = title.strip()
-        
-        # Obtenemos el país de origen
-        country = title_div.xpath('.//img[@class="nflag"]/@alt')[0]
-        
-        # Obtenemos el director y el reparto
-        director = div.xpath('.//div[@class="mc-director"]/div[@class="credits"]/span/a/text()')
-        cast = div.xpath('.//div[@class="mc-cast"]/div[@class="credits"]/span/a/text()')
-        
-        # Obtenemos el enlace de la película
-        link = div.xpath('.//div[@class="mc-title"]/a/@href')[0]
-        
-        movie_titles.append(title)
-        movie_years.append(year)
-        movie_countries.append(country)
-        movie_directors.append(", ".join(director))
-        movie_cast.append(", ".join(cast))
-        movie_links.append(link)
-    
-    # Guardo el valor de "movie_links" para devolverlo en el RETURN
-    obj.set_movie_links(movie_links)
-
-    # Recorremos todos los lis con la clase "data"
-    for li in root.xpath('//li[@class="data"]'):
-        # Extraemos la puntuación media y el número de puntuaciones
-        rating = li.xpath('.//div[@class="avg-rating"]/text()')[0]
-        rating_count = li.xpath('.//div[@class="rat-count"]/text()')[0].strip()
-        
-        movie_ratings.append(rating)
-        movie_rating_counts.append(rating_count)
-
-    # Escribimos todo en un archivo CSV
-    with open('movie_info_from_summary_page.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Título', 'Año', 'País', 'Puntuación Media', 'Número de Puntuaciones', "Director", "Reparto", "Enlace"])
-
-        for i, (title, year, country, rating, rating_count, director, cast, link) in enumerate(zip(movie_titles, movie_years, movie_countries, movie_ratings, movie_rating_counts, movie_directors, movie_cast, movie_links)):
-            # Detenemos el proceso si hemos escrito 1000 filas
-            if i >= 1000:
-                break
-            writer.writerow([title, year, country, rating, rating_count, director, cast, link])
-    return obj
+        print("Todos los hilos han terminado")
+   
+    return obj_pelicula
 
 
 
 
 
-# Ejecución
 
-obj_detalles_peliculas = ClasePeliculaDTO("",[])
+################## Ejecución ###################
+# tiempo inicial
 t0 = time.perf_counter_ns()
+
+# inicializo objetos de pelicula y web
 url = "https://www.filmaffinity.com/"
-current_url = login_FillmAffinity_And_Navegation_To_TopFA(url)
-html = return_html_after_scrape_movie_info_from_summary_page(current_url, 33)
+obj_detalles_peliculas = ClasePeliculaDTO("",[],[],[],[],[],[],[],[],[],[],[],[])
+
+# Abrir navegador Firefox y acceder a URL general
+options = webdriver.FirefoxOptions()
+options.headless = True
+
+# Instanciamos navegador Firefox con opciones definidas y navegamos a la URL deseada
+browser = webdriver.Firefox(options=options)
+browser.get(url)
+time.sleep(5)
+
+# Buscamos el botón «ACEPTO» y lo clicamos
+button = browser.find_element(By.CLASS_NAME, "css-v43ltw")
+button.click()
+
+# navegación a la sección "TopFA"
+irTopFA = browser.find_element(By.XPATH, '/html/body/header/div[2]/div/ul/li[1]/a').click()
+time.sleep(3)
+
+# Cargo la página "TopFA" con filtro de búsqueda concreto
+browser.get("https://www.filmaffinity.com/es/topgen.php?genres=&chv=0&orderby=avg&movietype=movie%7C&country=&fromyear=2013&toyear=2023&ratingcount=3&runtimemin=0&runtimemax=4")
+
+# realizo raspado inicial y guardo html devuelto
+html = return_html_after_scrape_movie_info_from_summary_page(browser, 33)
 obj_detalles_peliculas.set_html(html)
+
+# Tomo instantes de tiempo después del raspado inicial
 t1 = time.perf_counter_ns()
-tiempo_final_raspando = (t1 - t0) / 10**9
+tiempo_raspando_inicial = (t1 - t0) / 10**9
 
+# Recopilamos los datos del sumaryPage
+obj_detalles_peliculas = capture_data_from_sumaryPage(obj_detalles_peliculas)
 
-print("El tiempo que tardamos en raspar todos los datos generales es de: ",tiempo_final_raspando," (segundos)")
-objeto_devuelto= write_in_file_the_dataset(obj_detalles_peliculas)
+# Recopilamos los datos detallados de cada película
+obj_detalles_peliculas = multiHilos(obj_detalles_peliculas)
+
+# Creación del DATASET
+write_data_in_csv(obj_detalles_peliculas)
+
+# Tomamos tiempos midiento retardos escribiendo DATASET (csv)
 t2 = time.perf_counter_ns()
 tiempo_escribiendo_csv = (t2 - t1) / 10**9
+
+# Imprimo información de los costes temporales por consola
+print("El tiempo que tardamos en raspar todos los datos generales es de: ",tiempo_raspando_inicial," (segundos)")
 print("El tiempo que tardamos en volcar los datos raspados a fichero CSV: ",tiempo_escribiendo_csv," (segundos)")
 
-url = str(objeto_devuelto.get_movie_links()[0])
+# 
+url = str(obj_detalles_peliculas.get_movie_links()[0])
 html = captura_detalle_pelicula_HTML(url, "0.txt")
 
 
